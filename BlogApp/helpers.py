e6b39d91b8f3d602e90e475378111a9d2cad8695
NavcodeTech <77974772+NavcodeTech@users.noreply.github.com>
2021-11-23 09:53:36 +0530

    Create helpers.py
    added helpers file

---
+++ BlogApp/helpers.py
@@ -0,0 +1,34 @@
+from django.utils.text import slugify
+from urllib.parse import quote_plus
+import string
+import random
+
+def generate_random_string(N):
+    res = ''.join(random.choices(string.ascii_uppercase+string.digits, k = N))
+    return res
+
+def generate_slug(text):
+    new_slug = slugify(text)
+    from BlogApp.models import BlogModel
+    if BlogModel.objects.filter(slug = new_slug).first():
+        return generate_slug(text + generate_random_string(5))
+    return new_slug
+
+def likes_count(val):
+    print(val)
+    return val.count()
+
+def generate_string(title):
+    sharestring = quote_plus(title)
+    return sharestring
+
+
+from django.conf import settings
+from django.core.mail import send_mail
+def send_mail_to_user(token, email):
+    subject= f"You Account needs to be verified"
+    message= f"Hi, paste the link to verify account http://127.0.0.1:8000/verify/{token}"
+    email_from =settings.EMAIL_HOST_USER
+    recipient_list = [email]
+    send_mail(subject,message,email_from, recipient_list)
+    return True
